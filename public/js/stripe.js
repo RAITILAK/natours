ad0b9e51c2981411681021b953410ec0bf9ee480
RAITILAK <raitilak1212@gmail.com>
2024-06-01 13:26:10 +0530

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -11,9 +11,7 @@ export const bookTour = async (tourId) => {
 
     //1)get checkoutsession from api
 
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     console.log(session);
     //2) create checkout form + charge credit card
     await stripe.redirectToCheckout({
